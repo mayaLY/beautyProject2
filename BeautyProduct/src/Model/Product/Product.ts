acc30470471a5da3406a99112d1ba2a75d07ef40
orlyjonathan <168757564+orlyjonathan@users.noreply.github.com>
2024-12-05 00:43:53 +0200

    changes

--- BeautyProduct/src/Model/Product/Product.ts
+++ BeautyProduct/src/Model/Product/Product.ts
@@ -4,7 +4,6 @@ const productSchema = new mongoose.Schema({
     name: { type: String, required: true },
     price: { type: Number, required: true },
     description: { type: String, required: true },
-    category: { type: String, required: true },
     image: { type: String, required: true },
     stock: { type: Number, required: true },
 });
