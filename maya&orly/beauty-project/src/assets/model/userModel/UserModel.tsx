c4b4e07c8475d7acce574af843a53dfef36d823c
Mayalvy <mayalevy20@gmail.com>
2024-12-05 16:59:48 +0200

    update

---
+++ maya&orly/beauty-project/src/assets/model/userModel/UserModel.tsx
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    userID: { type: String, unique: true, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+  });
+
+  module.exports = mongoose.model("User", userSchema);
