674df23f0c2c803a40a76a68f584305389e7a71f
israelcohen1 <134219173+israelcohen1@users.noreply.github.com>
2024-12-09 20:51:30 +0200

    get products from public api

--- client/src/controllers/product/getAllProducts.ts
+++ client/src/controllers/product/getAllProducts.ts
@@ -1,8 +1,32 @@
 import { Product } from "../../model/productModel";
 
+const fetchProductsFromPublicApi = async (): Promise<Product[]> => {
+    try {
+        const response = await fetch('https://makeup-api.herokuapp.com/api/v1/products.json', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error(`Failed to fetch products from public API. Status: ${response.status}`);
+        }
+
+
+        const data = await response.json();
+        return data.products as Product[];
+        
+    } catch (error) {
+        console.error('Error fetching products from public API:', error);
+        throw error;
+    }
+
+}
+
 export const getAllProducts = async (): Promise<Product[]> => {
     try {
-        const response = await fetch('http://localhost:3000/api/clients/get-all-products', {
+        const response = await fetch('http://localhost:3006/api/clients/get-all-products', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -17,6 +41,6 @@ export const getAllProducts = async (): Promise<Product[]> => {
         return data as Product[];
     } catch (error) {
         console.error('Error fetching products:', error);
-        throw error; 
+        throw error;
     }
 };
