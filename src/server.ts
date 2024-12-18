472fd4eb52f4e68e14226eb75d538210bee72151
orlyjonathan <168757564+orlyjonathan@users.noreply.github.com>
2024-12-18 15:32:39 +0200

    changes

--- src/server.ts
+++ src/server.ts
@@ -28,7 +28,7 @@ mongoose.connect(`${DBurl}/${database}`).then(()=>{
 //routes
 import clientsRoutes from './routes/client/clientRoutes'
 app.use("/api/clients", clientsRoutes);
-import productRoutes from './routes/product/ProductRoutes';
+import productRoutes from './routes/product/productRoutes';
 app.use('/api/products', productRoutes);
 import cartRoutes from './routes/cart/cartRoutes';
 app.use('/api/cart', cartRoutes);
